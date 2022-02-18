eae3c54a6931d9647dc14f1df7433e51a3eb5ecc
CrisJimenez00 <cristina.del.carmen.jimenez.1@gmail.com>
2022-02-18 14:23:59 +0100

    Ejercicio matrices puesto bonito

--- src/main/java/ejerciciomatriz/MetodosMatriz.java
+++ src/main/java/ejerciciomatriz/MetodosMatriz.java
@@ -61,22 +61,22 @@ public class MetodosMatriz {
         ArrayList<Integer> lista = new ArrayList<>();
         System.out.println("El número centrar es: " + matriz[i][j]);
         int valor1 = 0, valor2 = 0, valor3 = 0, valor4 = 0, valor5 = 0, valor6 = 0, valor7 = 0, valor8 = 0;
-        valor1 = matriz[i - 1][j + 1];
-        valor2 = matriz[i - 1][j - 1];
-        valor3 = matriz[i + 1][j + 1];
-        valor4 = matriz[i + 1][j - 1];
+        valor1 = matriz[i - 1][j - 1];
+        valor2 = matriz[i - 1][j];
+        valor3 = matriz[i - 1][j + 1];
+        valor4 = matriz[i][j - 1];
         valor5 = matriz[i][j + 1];
-        valor6 = matriz[i][j - 1];
+        valor6 = matriz[i + 1][j - 1];
         valor7 = matriz[i + 1][j];
-        valor8 = matriz[i - 1][j];
-        lista.add(valor2);
-        lista.add(valor8);
+        valor8 = matriz[i + 1][j + 1];
         lista.add(valor1);
-        lista.add(valor6);
-        lista.add(valor5);
-        lista.add(valor4);
-        lista.add(valor7);
+        lista.add(valor2);
         lista.add(valor3);
+        lista.add(valor4);
+        lista.add(valor5);
+        lista.add(valor6);
+        lista.add(valor7);
+        lista.add(valor8);
 
         return lista;
     }
